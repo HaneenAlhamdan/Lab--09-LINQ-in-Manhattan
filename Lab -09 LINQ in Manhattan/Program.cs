de616888a69faa19e1da7ff67a77341127114d4a
Haneen M Al-Hamdan <henno_hamdan@yahoo.com>
2022-04-11 06:12:21 +0300

    Add project files.

---
+++ Lab -09 LINQ in Manhattan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab__09_LINQ_in_Manhattan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
